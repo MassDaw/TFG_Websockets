c1a295cf9bf55211024a3dd3f55adfc79f83f343
MassDaw <mcc606@educa.madrid.org>
2025-06-11 01:15:23 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,1 @@
-import asyncio
-import websockets
 
-async def test_client():
-    uri = "wss://tfg-websockets.onrender.com/ws"  # Cambia por tu URL real
-    async with websockets.connect(uri) as websocket:
-        message = await websocket.recv()
-        print(f"Received: {message}")
-
-asyncio.run(test_client())
